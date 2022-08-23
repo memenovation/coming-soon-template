43b6934b164326301c9f3013e6a39ddaaccc98df
P AC <ericson.tai@arch-partnership.com>
2022-08-23 19:05:32 +0800

    update: add next config to cater amplify

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    NEXT_PUBLIC_SITE_TITLE: process.env.NEXT_PUBLIC_SITE_TITLE,
+    AIRTABLE_ENDPOINT: process.env.AIRTABLE_ENDPOINT,
+    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+  },
+};
